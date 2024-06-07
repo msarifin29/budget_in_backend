60fe271b68b476397426bd233351b70608efa557
samsul <ms.arifin29@gmail.com>
2024-06-07 14:32:18 +0800

    fix : balance repository

--- internal/repository/balance_repository.go
+++ internal/repository/balance_repository.go
@@ -21,7 +21,7 @@ type BalanceRepositoryImpl struct {
 // GetBalance implements BalanceRepository.
 func (*BalanceRepositoryImpl) GetBalance(ctx context.Context, tx *sql.Tx, uid string) (float64, error) {
 	var n float64
-	row := tx.QueryRowContext(ctx, `select balance from users where uid = ?`, uid)
+	row := tx.QueryRowContext(ctx, `select balance from users where uid = $1`, uid)
 	err := row.Scan(&n)
 	return n, err
 }
@@ -29,7 +29,7 @@ func (*BalanceRepositoryImpl) GetBalance(ctx context.Context, tx *sql.Tx, uid st
 // GetCash implements BalanceRepository.
 func (*BalanceRepositoryImpl) GetCash(ctx context.Context, tx *sql.Tx, uid string) (float64, error) {
 	var n float64
-	row := tx.QueryRowContext(ctx, `select cash from users where uid = ?`, uid)
+	row := tx.QueryRowContext(ctx, `select cash from users where uid = $1`, uid)
 	err := row.Scan(&n)
 	return n, err
 }
@@ -37,7 +37,7 @@ func (*BalanceRepositoryImpl) GetCash(ctx context.Context, tx *sql.Tx, uid strin
 // GetDebt implements BalanceRepository.
 func (*BalanceRepositoryImpl) GetDebt(ctx context.Context, tx *sql.Tx, uid string) (float64, error) {
 	var n float64
-	row := tx.QueryRowContext(ctx, `select debts from users where uid = ?`, uid)
+	row := tx.QueryRowContext(ctx, `select debts from users where uid = $1`, uid)
 	err := row.Scan(&n)
 	return n, err
 }
@@ -45,33 +45,33 @@ func (*BalanceRepositoryImpl) GetDebt(ctx context.Context, tx *sql.Tx, uid strin
 // GetSaving implements BalanceRepository.
 func (*BalanceRepositoryImpl) GetSaving(ctx context.Context, tx *sql.Tx, uid string) (float64, error) {
 	var n float64
-	row := tx.QueryRowContext(ctx, `select savings from users where uid = ?`, uid)
+	row := tx.QueryRowContext(ctx, `select savings from users where uid = $1`, uid)
 	err := row.Scan(&n)
 	return n, err
 }
 
 // SetBalance implements BalanceRepository.
 func (*BalanceRepositoryImpl) SetBalance(ctx context.Context, tx *sql.Tx, uid string, i float64) error {
-	_, err := tx.ExecContext(ctx, `update users set balance = ? where uid = ?`, i, uid)
+	_, err := tx.ExecContext(ctx, `update users set balance = ? where uid = $1`, i, uid)
 	return err
 }
 
 // SetCash implements BalanceRepository.
 func (*BalanceRepositoryImpl) SetCash(ctx context.Context, tx *sql.Tx, uid string, i float64) error {
-	_, err := tx.ExecContext(ctx, `update users set cash = ? where uid = ?`, i, uid)
+	_, err := tx.ExecContext(ctx, `update users set cash = ? where uid = $1`, i, uid)
 	return err
 }
 
 // SetDebt implements BalanceRepository.
 func (*BalanceRepositoryImpl) SetDebt(ctx context.Context, tx *sql.Tx, uid string, i float64) error {
-	_, err := tx.ExecContext(ctx, `update users set debts = ? where uid = ?`, i, uid)
+	_, err := tx.ExecContext(ctx, `update users set debts = ? where uid = $1`, i, uid)
 	return err
 
 }
 
 // SetSaving implements BalanceRepository.
 func (*BalanceRepositoryImpl) SetSaving(ctx context.Context, tx *sql.Tx, uid string, i float64) error {
-	_, err := tx.ExecContext(ctx, `update users set savings = ? where uid = ?`, i, uid)
+	_, err := tx.ExecContext(ctx, `update users set savings = ? where uid = $1`, i, uid)
 	return err
 }
 
