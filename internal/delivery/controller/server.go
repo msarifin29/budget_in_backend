73873a1cd8c13c1e91ce715f55f6bb29a537d70e
samsul <ms.arifin29@gmail.com>
2024-06-15 15:04:53 +0800

    remove : feature depreceted credit

--- internal/delivery/controller/server.go
+++ internal/delivery/controller/server.go
@@ -12,14 +12,13 @@ import (
 )
 
 type Server struct {
-	Log        *logrus.Logger
-	Engine     *gin.Engine
-	Con        config.Config
-	TokenMaker util.Maker
-	UserC      UserController
-	ExpenseC   ExpenseController
-	IncomeC    IncomeController
-	// CreditC      CreditController *Depreceted Will be remove later
+	Log          *logrus.Logger
+	Engine       *gin.Engine
+	Con          config.Config
+	TokenMaker   util.Maker
+	UserC        UserController
+	ExpenseC     ExpenseController
+	IncomeC      IncomeController
 	AccountC     AccountController
 	MonthReportC MonthlyReportController
 	PrivacyC     PrivacyController
@@ -38,7 +37,6 @@ func NewServer(Log *logrus.Logger, Con config.Config) (*Server, error) {
 	expenseRepo := repository.NewExpenseRepository()
 	balanceRepo := repository.NewBalanceRepository()
 	incomeRepo := repository.NewIncomeRepository()
-	// creditRepo := repository.NewCreditRepository()
 	accountRepo := repository.NewAccountRepository()
 	monthlyRepo := repository.NewMonthlyRepository()
 	categoryRepo := repository.NewCategoryRepository()
@@ -47,7 +45,6 @@ func NewServer(Log *logrus.Logger, Con config.Config) (*Server, error) {
 	userUsecase := usecase.NewUserUsecase(userRepo, accountRepo, Log, db, Con)
 	expenseUseCase := usecase.NewExpenseUsecase(categoryRepo, expenseRepo, balanceRepo, accountRepo, Log, db)
 	incomeUsecase := usecase.NewIncomeUsecase(incomeRepo, balanceRepo, accountRepo, Log, db, categoryRepo)
-	// creditUsecase := usecase.NewCreditUsecase(creditRepo, balanceRepo, accountRepo, Log, db, expenseRepo, categoryRepo)
 	accountUsacase := usecase.NewAccountUsacase(accountRepo, expenseRepo, Log, db)
 	monthlyUsecase := usecase.NewMonthlyReportUsecase(monthlyRepo, Log, db)
 
@@ -55,19 +52,17 @@ func NewServer(Log *logrus.Logger, Con config.Config) (*Server, error) {
 	userController := NewUserController(userUsecase, Log, Con, tokenMaker)
 	expenseController := NewExpenseController(expenseUseCase, Log)
 	incomeController := NewIncomeController(incomeUsecase, Log)
-	// creditController := NewCreditController(creditUsecase, Log)
 	accountController := NewAccountController(accountUsacase, Log)
 	monthlyController := NewMonthlyController(monthlyUsecase, Log)
 	privacyController := NewPrivacyController(Log)
 
 	server := &Server{
-		Log:        Log,
-		Con:        Con,
-		TokenMaker: tokenMaker,
-		UserC:      *userController,
-		ExpenseC:   *expenseController,
-		IncomeC:    *incomeController,
-		// CreditC:      *creditController,
+		Log:          Log,
+		Con:          Con,
+		TokenMaker:   tokenMaker,
+		UserC:        *userController,
+		ExpenseC:     *expenseController,
+		IncomeC:      *incomeController,
 		AccountC:     *accountController,
 		MonthReportC: *monthlyController,
 		PrivacyC:     *privacyController,
--- internal/repository/category_repository.go
+++ internal/repository/category_repository.go
@@ -18,13 +18,6 @@ type CategoryRepository interface {
 
 type CategoryRepositoryImpl struct{}
 
-// CreateCategoryCredits implements CategoryRepository.
-// func (CategoryRepositoryImpl) CreateCategoryCredits(ctx context.Context, tx *sql.Tx, category model.Category) (model.Category, error) {
-// 	script := `insert into t_category_credits (category_id,id,title) values (?,?,?)`
-// 	_, err := tx.ExecContext(ctx, script, category.CategoryId, category.Id, category.Title)
-// 	return category, err
-// }
-
 // CreateCategoryExpense implements CategoryRepository.
 func (CategoryRepositoryImpl) CreateCategoryExpense(ctx context.Context, tx *sql.Tx, category model.Category) (model.Category, error) {
 	script := `insert into t_category_expenses (category_id,id,title) values ($1,$2,$3)`
@@ -38,15 +31,6 @@ func (CategoryRepositoryImpl) CreateCategoryIncomes(ctx context.Context, tx *sql
 	_, err := tx.ExecContext(ctx, script, category.CategoryId, category.Id, category.Title)
 	return category, err
 }
-
-// GetCategoryCredit implements CategoryRepository.
-// func (CategoryRepositoryImpl) GetCategoryCredit(ctx context.Context, tx *sql.Tx, categoryId float64) (model.Category, error) {
-// 	script := `select category_id, id, title from t_category_credits where category_id = ?`
-// 	row := tx.QueryRowContext(ctx, script, categoryId)
-// 	var category model.Category
-// 	err := row.Scan(&category.CategoryId, &category.Id, &category.Title)
-// 	return category, err
-// }
 
 // GetCategoryExpense implements CategoryRepository.
 func (CategoryRepositoryImpl) GetCategoryExpense(ctx context.Context, tx *sql.Tx, categoryId float64) (model.Category, error) {
