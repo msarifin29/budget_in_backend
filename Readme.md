2dbce6284fbaf371b5e416cb195012a0c977e62b
samsul <ms.arifin29@gmail.com>
2024-06-17 14:01:41 +0800

    fix : flavor and update readme

--- Readme.md
+++ Readme.md
@@ -1,1 +1,28 @@
 # go version 1.21.5
+
+# Development
+
+## Build and Run 
+
+1. `go build -o bin/dev-app ./cmd/dev`
+2. `nohup ./bin/dev-app &> dev-app.log &`
+
+## Check Running Processes 
+`ps aux | grep dev-app` or `pgrep -fl dev-app`
+
+## Killing the Process 
+first find PID `pgrep dev-app`
+then kill PID `kill <PID>`
+
+# Production
+
+## Build and Run 
+1. `go build -o bin/prod-app ./cmd/prod`
+2. `nohup ./bin/pro-app &> prod-app.log &`
+
+## Check Running Processes 
+`ps aux | grep pro-app` or `pgrep -fl pro-app`
+
+## Killing the Process 
+first find PID `pgrep prod-app`
+then kill PID `kill <PID>`
--- cmd/dev/main_dev.go
+++ cmd/dev/main_dev.go
@@ -7,17 +7,24 @@ import (
 	"github.com/msarifin29/be_budget_in/internal/delivery/controller"
 )
 
+var env, port string
+
+func init() {
+	env = "dev"
+	port = ":8080"
+}
+
 func main() {
 	logger := config.NewLogger()
-	con, errCon := config.LoadConfig("../..", "dev")
+	con, errCon := config.LoadConfig("/home/samsul-dev/projects/budget_in_backend", env)
 	if errCon != nil {
 		log.Fatalf("cannot load config %e :", errCon)
 	}
-	server, err := controller.NewServer(logger, con, "dev")
+	server, err := controller.NewServer(logger, con, env)
 	if err != nil {
 		log.Fatalf("cannot create server %e :", errCon)
 	}
-	errStart := server.Start(con.ServerAddress)
+	errStart := server.Start(con.ServerAddress + port)
 	if errStart != nil {
 		log.Fatalf("cannot start server %e :", errStart)
 	}
